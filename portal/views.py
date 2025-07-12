4682f0df5e6d9be311605da4c367e95633e3a8ad
Carlos Moreno Ribeiro <omorenodovale@gmail.com>
2025-07-12 14:32:01 -0300

    feat: Update select tag

--- portal/views.py
+++ portal/views.py
@@ -16,32 +16,22 @@ def index(tag=None):
     Se uma tag for fornecida, filtra as dicas por essa tag.
     Caso contrário, mostra todas as dicas.
     """
-    # Pega a lista de todas as tags únicas para exibir os botões de filtro
     all_tags = get_unique_tags()
 
     if tag:
-        # Se uma tag foi passada na URL, busca as dicas por essa tag
         tips_list = get_tips_by_tag(tag)
         page_title = f'Dicas com a tag: {tag}'
     else:
-        # Se nenhuma tag foi passada, busca todas as dicas
         tips_list = get_all_tips()
         page_title = 'Todas as Dicas'
 
     return render_template(
-        'index.html.j2',  # Nome do seu arquivo de template
+        'index.html.j2',
         tips=tips_list,
-        all_tags=all_tags,
+        all_tags=sorted(all_tags),
         title=page_title,
+        active_tag=tag,
     )
-
-
-# @bp.route("/tag")
-# def detail(tag):
-#     tips = get_tips_by_tag(tag)
-#     if not post:
-#         return abort(404, "Page not found")
-#     return render_template("post.html.j2", tips=tips)
 
 
 def configure(app):
